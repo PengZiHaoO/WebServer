4c62142e984f3e43754567896b977f01cde2feec
root <root@LAPTOP-O3KVR4K7.localdomain>
2022-07-01 14:38:39 +0800

    add threadpool

---
+++ threadpool/threadpool.h
@@ -0,0 +1,31 @@
+#ifndef THREADPOOL_H
+#define THREADPOOL_H
+
+#include <list>
+#include <cstdio>
+#include <exception>
+#include "../locker/locker.hpp"
+/*#include "../CGImysql/sql_connection_pool.h"*/
+
+template<class T>
+class ThreadPool{
+public:
+    ThreadPool(int actor_model,/*connection_pool *conn_pool,*/int thread_number = 8,int max_request_number = 10000);
+    ~ThreadPool();
+    bool append(T* request, int state);
+    bool append_p(T* request);
+private:   
+    static void* worker(void* args);
+    void run();
+private:
+    int m_thread_number;
+    int m_max_requests_number;
+    pthread_t* m_threads;
+    std::list<T* >m_worker_queue;
+    Mutex m_queue_mutex;
+    Semaphore m_queue_state;
+    //connection_pool* m_conn_pool;
+    int m_actor_model;
+};
+
+#endif
