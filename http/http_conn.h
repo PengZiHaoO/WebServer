3eb7879e4d544612e8548174e4defbe25069562e
root <root@LAPTOP-O3KVR4K7.localdomain>
2022-07-03 01:26:19 +0800

    \0

--- http/http_conn.h
+++ http/http_conn.h
@@ -84,6 +84,40 @@ public:
     int improv;
 
 private:
-    
+    void init();
+    HTTP_CODE process_read();
+private:
+    int m_sockfd;
+    sockaddr_in m_address;
+    char m_read_buf[READ_BUFFER_SIZE];
+    int m_read_idx;
+    int m_checked_idx;
+    int m_start_line;
+    char m_write_buf[WRITE_BUFFER_SIZE];
+    int m_write_idx;
+    CHECK_STATE m_check_state;
+    METHOD m_method;
+    char real_file[FILE_NAME_LEN];
+    char* m_url;
+    char* m_version;
+    char* m_host;
+    int m_content_length;
+    bool m_linger;
+    char* m_file_address;
+    struct stat m_file_stat;
+    iovec m_iv[2];
+    int m_iv_count;
+    int cgi;
+    char* m_string;
+    int bytes_to_send;
+    int bytes_have_send;
+    char* doc_root;
+    std::map<std::string, std::string> m_users;
+    int m_trig_mode;
+    int m_close_log;
+    char sql_user[100];
+    char sql_passwd[100];
+    char sql_name[100];
 };
+
 #endif
