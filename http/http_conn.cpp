11d7de1ee7599add96d2d7440e0179a2295ed0fd
root <root@LAPTOP-O3KVR4K7.localdomain>
2022-07-11 16:27:20 +0800

    do_process

--- http/http_conn.cpp
+++ http/http_conn.cpp
@@ -64,6 +64,10 @@ void modfd(int epfd, int fd, int ev, int trig_mode){
     epoll_ctl(epfd, EPOLL_CTL_MOD, fd, &event);
 }
 
+sockaddr_in* HTTPConnection::get_address(){
+    return &m_address;
+}
+
 int HTTPConnection::m_epollfd = -1;
 int HTTPConnection::m_user_count = 0;
 
@@ -402,3 +406,142 @@ HTTPConnection::HTTP_CODE HTTPConnection::parse_content(char* text){
     return NO_REQUEST;
 }
 
+
+char* HTTPConnection::get_line(){
+    return (m_read_buf + m_start_line);
+}
+
+
+HTTPConnection::HTTP_CODE HTTPConnection::do_request(){
+    strcpy(m_real_file, doc_root);
+
+    int len = strlen(doc_root);
+
+    const char* p = strrchr(m_url, '/');
+
+    //cgi判读method
+    if(cgi == 1 && (*(p + 1) == '2') || *(p + 1) == '3'){
+        char flag = m_url[1];
+
+        char *m_url_real = (char *)malloc(sizeof(char) * 200);
+
+        strcpy(m_url_real, "/");
+        strcat(m_url_real, m_url + 2);
+        strncpy(m_real_file + len, m_url_real, FILE_NAME_LEN - len -1);
+
+        free(m_url_real);
+
+        //提取usr name passwd
+        //usr name passwd存在m_string中,"&"分隔
+        char usr_name[100];
+        char passwd[100];
+
+        int i;
+        for(i = 5; m_string[i] != '&'; ++i){
+            usr_name[i - 5] = m_string[i];
+        }
+        usr_name[i - 5] = '\0';
+
+        int j;
+        for(i = i + 10; m_string[i] != '\0'; ++i, ++j){
+            passwd[j] = m_string[i];
+        }
+        passwd[j] = '\0';
+
+        if(*(p + 1) == '3'){
+            //'3' 未注册
+            //首先判断db中是否有重名
+            //若无则注册
+            char *sql_insert = (char *)malloc(sizeof(char) * 200);
+            strcpy(sql_insert, "INSERT INTO user(username, passwd) VALUES(");
+            strcat(sql_insert, "'");
+            strcat(sql_insert, usr_name);
+            strcat(sql_insert, "', '");
+            strcat(sql_insert, passwd);
+            strcat(sql_insert, "')");
+
+            //usr_name未重复
+            if(users.find(usr_name) == users.end()){
+                mutex.lock();
+                int res = mysql_query(m_mysql, sql_insert);
+                users.insert(std::pair<std::string, std::string>(usr_name, passwd));
+                mutex.unlock();
+
+                if(!res){
+                    strcpy(m_url, "/log.html");
+                }
+                else{
+                    strcpy(m_url, "/registerError.html");
+                }
+            }
+            //重复
+            else{
+                strcpy(m_url, "/registerError.html");
+            }
+        }
+        //登录
+        //查询表判断数据
+        else if(*(p + 1) == '2'){
+            if (users.find(usr_name) != users.end() && users[usr_name] == passwd)
+                strcpy(m_url, "/welcome.html");
+            else
+                strcpy(m_url, "/logError.html");
+        }
+    }
+    if(*(p + 1) == '0'){
+        char *m_url_real = (char *)malloc(sizeof(char) * 200);
+        strcpy(m_url_real, "/register.html");
+        strncpy(m_real_file + len, m_url_real, strlen(m_url_real));
+
+        free(m_url_real);
+    }
+    else if(*(p + 1) == '1'){
+        char *m_url_real = (char *)malloc(sizeof(char) * 200);
+        strcpy(m_url_real, "/log.html");
+        strncpy(m_real_file + len, m_url_real, strlen(m_url_real));
+
+        free(m_url_real);
+    }
+    else if(*(p + 1) == '5'){
+        char *m_url_real = (char *)malloc(sizeof(char) * 200);
+        strcpy(m_url_real, "/picture.html");
+        strncpy(m_real_file + len, m_url_real, strlen(m_url_real));
+
+        free(m_url_real);
+    }
+    else if(*(p + 1) == '6'){
+        char *m_url_real = (char *)malloc(sizeof(char) * 200);
+        strcpy(m_url_real, "/video.html");
+        strncpy(m_real_file + len, m_url_real, strlen(m_url_real));
+
+        free(m_url_real);
+    }
+    else if(*(p + 1) == '7'){
+        char *m_url_real = (char *)malloc(sizeof(char) * 200);
+    strcpy(m_url_real, "/fans.html");
+    strncpy(m_real_file + len, m_url_real, strlen(m_url_real));
+
+    free(m_url_real);
+    }
+    else{
+        strncpy(m_real_file + len, m_url, FILE_NAME_LEN - len - 1);
+    }
+
+    if(stat(m_real_file, &m_file_stat) < 0){
+        return NO_RESOURCE;
+    }
+
+    if(!(m_file_stat.st_mode & S_IROTH)){
+        return BAD_REQUEST;
+    }
+
+    if(S_ISDIR(m_file_stat.st_mode)){
+        return BAD_REQUEST;
+    }
+
+    int fd = open(m_real_file, O_RDONLY);
+    m_file_address = (char *)mmap(0, m_file_stat.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    close(fd);
+
+    return FILE_REQUEST;
+}
